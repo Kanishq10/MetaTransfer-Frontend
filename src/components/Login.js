82a6cf2bb79cc21121ce85eec5a6aaafc3afdfb4
Hrishabh963 <80190103+Hrishabh963@users.noreply.github.com>
2022-11-27 23:14:52 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -15,7 +15,7 @@ const Login = () => {
     const loginUser = async (event) => {
         event.preventDefault()
 
-        const response = await fetch("https://busy-pink-earthworm-cap.cyclic.app/login", {
+        const response = await fetch("https://busy-pink-earthworm-cap.cyclic.app/api/login", {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json'
